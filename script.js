b657c94fd9e9a660ede95486e3d9b0d14cbf7529
almazu <almazuleyma@gmail.com>
2020-09-26 23:12:36 -0700

    Refactored code, updated style

--- script.js
+++ script.js
@@ -1,69 +1,91 @@
-// declare minutes
-var mins = 2;
+var secs = 0;
+var mins = 0;
+var interval;
 
-// calculate seconds
-var secs = mins * 60;
+minutesBox = document.querySelector('.minutes');
+secondsBox = document.querySelector('.seconds');
 
-// countdown function runs when Start button is clicked
-function countdown() {
-    setTimeout('decrement()', 60);
-}
-
-// decrement functions runs when called by countdown()
-// decrements timer
-function decrement() {
-    if (document.getElementById) {
-        minutes = document.querySelector('.minutes');
-        seconds = document.querySelector('.seconds');
-    }
-
-    // display seconds if less than a minute
-    if (seconds < 59) {
-        seconds.value = secs;
-    } else {
-        // display minutes and seconds
-        minutes.value = getMinutes();
-        seconds.value = getSeconds();
-    }
-
-
-
-    // alert if seconds become zero
-    if (mins < 0) {
-        alert('times up');
-        minutes.value = 0;
-        seconds.value = 0;
-    } else {
-        // decrement seconds
-        secs--;
-        setTimeout('decrement()', 1000);
-    }
-}
-
-// getMinutes function gets minutes
+// function gets minutes
 function getMinutes() {
     mins = Math.floor(secs / 60);
     return mins;
 }
 
-// getSeconds function gets seconds
+// function gets seconds
 function getSeconds() {
     return secs - Math.round(mins * 60);
 }
 
-function togglePause(element) {
-    if (element.className == 'pause-btn') {
-        element.className = 'start-btn';
-        element.innerHTML = 'Start';
-    } else {
-        element.className = 'pause-btn';
-        element.value = 'Pause';
-        element.innerHTML = 'Pause';
-    }
+// function changes background color
+function changeBackgroundColor(color) {
+    document.body.style.background = color;
 }
+// Start button event listener
+const workBtn = document.querySelector('.work-btn');
+workBtn.addEventListener('click', event => {
+    // change background color
+    changeBackgroundColor("linear-gradient(to bottom right, #FF0401, #820541)");
 
+    // declare minutes
+    var mins = 52;
+
+    // calculate seconds
+    secs = mins * 60;
+
+    interval = setInterval(function () {
+        // decrement seconds
+        secs--;
+
+        // display minutes and seconds
+        // if less than ten include a zero in the tenths place
+        minutesBox.value = mins < 10 ? "0" + getMinutes() : getMinutes();
+        secondsBox.value = getSeconds() < 10 ? ":0" + getSeconds() : ":" + getSeconds();
+
+        // end when seconds reach zero
+        // alert time up
+        if (!secs) {
+            clearInterval(interval, 1000);
+            alert("Time's up! Take a break.");
+        }
+
+    }, 1000)
+});
+
+// Break button event listener
 // event listener runs countdown() function when Start button is clicked
-const startBtn = document.querySelector('.start-btn');
-startBtn.addEventListener('click', event => {
-    countdown();
+const breakBtn = document.querySelector('.break-btn');
+breakBtn.addEventListener('click', event => {
+    // change background color
+    changeBackgroundColor("linear-gradient(to bottom right, #7a7fba, #11c37c)");
+
+    // declare minutes
+    var mins = 17;
+
+    // calculate seconds
+    secs = mins * 60;
+
+    interval = setInterval(function () {
+        // decrement seconds
+        secs--;
+
+        // display minutes and seconds
+        // if less than ten include a zero in the tenths place
+        minutesBox.value = mins < 10 ? "0" + getMinutes() : getMinutes();
+        secondsBox.value = getSeconds() < 10 ? ":0" + getSeconds() : ":" + getSeconds();
+
+        // end when seconds reach zero
+        // alert time up
+        if (!secs) {
+            clearInterval(interval, 1000);
+            alert("Time's up! Get to work.");
+        }
+
+    }, 1000)
+});
+
+// Stop button event listener
+const stopBtn = document.querySelector('.stop-btn');
+stopBtn.addEventListener('click', event => {  
+    changeBackgroundColor("linear-gradient(grey,silver)");
+    clearInterval(interval);
 });
